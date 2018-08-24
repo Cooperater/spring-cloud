5123449ff0632e545da23fd334ae88c25acea48a
luanxw <luan_xw@qq.com>
2018-08-24 15:28:32 +0800

    zuul网关服务的简单实现。

--- luan-client/src/main/java/com/app/test/TestController.java
+++ luan-client/src/main/java/com/app/test/TestController.java
@@ -1,4 +1,4 @@
-package com.app.test;
+ package com.app.test;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +13,7 @@ public class TestController {
 	@GetMapping("hello")
 	public String hello(@RequestParam String name){
 		System.out.println("现在是luan-celient,端口号"+port+"为您服务：");
-		String result = "hi !"+name+" 现在是luan-celient,端口号"+port+"为您服务";
+		String result = "hi ! "+name+" 现在是luan-celient,端口号"+port+"为您服务";
 		return result;
 		
 	}
